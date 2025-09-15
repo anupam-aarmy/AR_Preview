9352ce0a0d9c421da170f55376d915e88af6af02
Anupam Abhay <anupam@assistarmy.in>
2025-09-15 18:55:46 +0530

    created main script boilerplate

---
+++ main.py
@@ -0,0 +1,15 @@
+import cv2
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+from segment_anything import sam_model_registry, SamAutomaticMaskGenerator
+
+def main():
+    print("Pipeline starting...")
+
+    # Logic
+
+    print("Pipeline complete.")
+
+if __name__ == "__main__":
+    main()
